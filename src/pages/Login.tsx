0d59cff2963e36f85ccec4daf2543c26e3669523
muzi5@kakao.com <muzi5@kakao.com>
2024-10-20 14:44:21 +0900

    로그인 구현, 토큰 저장

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,101 @@
 import { useId, useState } from 'react'
 import TextField from '../components/textfield/TextField'
+import useInputError from '../hooks/useInputError'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+  const navigate = useNavigate()
   const emailId = useId()
   const passwordId = useId()
 
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
 
+  const {
+    errorState,
+    emailErrorState,
+    emailCheck,
+    emailCheckPatten,
+    passwordErrorState,
+    passwordCheck,
+    rePasswordErrorState,
+    passwordReCheck,
+    disableButton,
+  } = useInputError()
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    emailErrorState(null)
+    setEmail(value)
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    passwordErrorState(null)
+    setPassword(value)
+  }
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const bodyData = {
+        email: email,
+        password: password,
+      }
+
+      const res = await axios.post(
+        `${import.meta.env.VITE_SERVER_API}/users/login`,
+        bodyData
+      )
+      if (res.data.token) {
+        localStorage.setItem('token', res.data.token)
+        navigate('/')
+      }
+      console.log(res)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <section className="w-100">
       <h1>Login</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <TextField
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
+          onChange={handleEmailChange}
+          placeholder="이메일"
           type="email"
-          name="login"
+          name="email"
           id={`email-${emailId}`}
           label="Email"
           htmlFor={`email-${emailId}`}
+          isError={!!errorState.email}
+          onBlur={() => {
+            emailCheck(email)
+          }}
+          errorMessage={emailCheckPatten(errorState.email!)}
         />
+
         <TextField
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          onChange={handlePasswordChange}
+          placeholder="비밀번호"
           type="password"
           name="password"
           id={`password-${passwordId}`}
           label="Password"
           htmlFor={`password-${passwordId}`}
+          onBlur={() => passwordCheck(password)}
+          isError={!!errorState.password}
+          errorMessage={
+            errorState.password === 'emptyInput'
+              ? '비밀번호를 입력해주세요.'
+              : errorState.password === 'invalidFormat'
+                ? '비밀번호는 8자 이상이어야 합니다.'
+                : ''
+          }
         />
         <button type="submit">Login</button>
       </form>
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  useNavigate,
+} from 'react-router-dom'
 import Login from '../pages/Login'
 import Auth from '../pages/Auth'
+import Home from '../pages/Home'
 
 function Router() {
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+  console.log('token', token)
+  useEffect(() => {
+    if (!token || token === 'undefined') {
+      alert(`토큰이 없습니다.
+로그인 페이지로 이동합니다.`)
+      navigate('/auth')
+      return
+    }
+  }, [])
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </>
   )
 }
